4756e100da3f820b3bbb441bee004925a74816ac
PC-SIMON\Simon <sim.chambonnet@gmail.com>
2016-12-02 08:26:14 +0100

    modif ProdCons

--- src/jus/proc/prodcons/v1/Consommateur.java
+++ src/jus/proc/prodcons/v1/Consommateur.java
@@ -1,5 +1,36 @@
 package jus.proc.prodcons.v1;
 
-public class Consommateur {
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Tampon;
+import jus.poc.prodcons._Consommateur;
+import jus.poc.prodcons._Producteur;
+
+public class Consommateur implements Tampon {
+	
+	int nbMessage = 0;
+
+	@Override
+	public int enAttente() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public synchronized Message get(_Consommateur arg0) throws Exception, InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public synchronized void put(_Producteur arg0, Message arg1) throws Exception, InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int taille() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
