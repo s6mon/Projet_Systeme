6b04a8abb67d7424f2c7810e50dfc046dfc70da7
PC-SIMON\Simon <sim.chambonnet@gmail.com>
2016-12-12 21:56:51 +0100

    suppression listener v2

--- src/jus/poc/prodcons/v2/EtatProdListener.java
+++
@@ -1,8 +0,0 @@
-package jus.poc.prodcons.v2;
-
-import java.util.EventListener;
-
-public interface EtatProdListener extends EventListener {
-	void etatProdChangee(boolean oldValue, boolean newValue);
-
-}
